8f6dde78a7f8cac9aae6437e9f9869ba345d307d
KriptoBaba382 <199w29c@mail.ru>
2025-09-16 10:22:19 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let title = document.getElementsByTagName('h1')[0];
+let buttons = document.getElementsByClassName('handler_btn');
+let screen = document.querySelector('.screen-btn');
+let other = document.querySelectorAll('.other-items.percent');
+let num = document.querySelectorAll('.other-items.number');
+const rangeInput = document.querySelector('.rollback input[type="range"]');
+let rangeSpan = document.querySelector('.rollback span.range-value');
+let totalInput = document.getElementsByClassName('total-input');
+let blockScreen = document.querySelectorAll('screen');
+
